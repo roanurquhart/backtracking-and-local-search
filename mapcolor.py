8e38386a0e5baffe734c7c1e11ba4a00adb1bf11
roanurquhart <roanurquhart@gmail.com>
2020-02-02 15:08:44 -0500

    finished refactor

--- mapcolor.py
+++ mapcolor.py
@@ -1,60 +1,11 @@
 import sys
 import utilityfuncs
 
-with open(sys.argv[1], "r") as ausfile:
-    data = ausfile.readlines()
-    print(data)
+with open(sys.argv[1], "r") as statefile:
+    data = statefile.readlines()
 
-
-class State:
-    def __init__(self, name):
-        self.name = name
-        self.adjacentStates = []
-
-    def __repr__(self):
-        return "<State name:%s adjacentStates:%s>" % (self.name, self.adjacentStates)
-
-    def __str__(self):
-        return "Test: name:%s, adjacentStates:%s" % (self.name, self.adjacentStates)
-
-    def get_name(self):
-        return self.name
-
-
-inputIncrement = 0
-counter = 0
-colors = []
-states = {}
-while inputIncrement < 3:
-    while inputIncrement == 0:
-        if data[counter] != '\n':
-            colors.append(data[counter].rstrip())
-            counter += 1
-        else:
-            counter += 1
-            inputIncrement += 1
-    while inputIncrement == 1:
-        if data[counter] != '\n':
-            stateName = data[counter].rstrip()
-            state = State(stateName)
-            states[stateName] = state
-            counter += 1
-        else:
-            counter += 1
-            inputIncrement += 1
-    while inputIncrement == 2:
-        if counter < len(data):
-            connection = data[counter].split()
-            stateOne = states[connection[0]]
-            stateTwo = states[connection[1]]
-            stateOne.adjacentStates.append(stateTwo)
-            stateTwo.adjacentStates.append(stateOne)
-            counter += 1
-        else:
-            counter += 1
-            inputIncrement += 1
-
-utilityfuncs.print_connections((states.values()))
+utilityfuncs.parse_input(data)
+utilityfuncs.print_connections((utilityfuncs.states.values()))
 
 
 
--- utilityfuncs.py
+++ utilityfuncs.py
@@ -1,4 +1,54 @@
 
+colors = []
+states = {}
+
+class State:
+    def __init__(self, name):
+        self.name = name
+        self.adjacentStates = []
+
+    def __repr__(self):
+        return "<State name:%s adjacentStates:%s>" % (self.name, self.adjacentStates)
+
+    def __str__(self):
+        return "Test: name:%s, adjacentStates:%s" % (self.name, self.adjacentStates)
+
+    def get_name(self):
+        return self.name
+
+
+def parse_input(data):
+    input_increment = 0
+    counter = 0
+    while input_increment < 3:
+        while input_increment == 0:
+            if data[counter] != '\n':
+                colors.append(data[counter].rstrip())
+                counter += 1
+            else:
+                counter += 1
+                input_increment += 1
+        while input_increment == 1:
+            if data[counter] != '\n':
+                state_name = data[counter].rstrip()
+                state = State(state_name)
+                states[state_name] = state
+                counter += 1
+            else:
+                counter += 1
+                input_increment += 1
+        while input_increment == 2:
+            if counter < len(data):
+                connection = data[counter].split()
+                state_one = states[connection[0]]
+                state_two = states[connection[1]]
+                state_one.adjacentStates.append(state_two)
+                state_two.adjacentStates.append(state_one)
+                counter += 1
+            else:
+                counter += 1
+                input_increment += 1
+
 
 def print_connections(states):
     for location in states:
